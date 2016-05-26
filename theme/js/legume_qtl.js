67f737373dc0f7a878f99ae13caaefa51fe7e8ed
E.Cannon <ekcannon@iastsate.edu>
2016-05-26 11:19:53 -0500

    initial import

---
+++ theme/js/legume_qtl.js
@@ -0,0 +1,10 @@
+(function($) {
+  $(document).ready(function() {
+    if($(".views-submit-button")) {
+      $(".views-submit-button").css('float', 'none');
+    }
+    if ($("#edit-submit-qtl")) {
+      $("#edit-submit-qtl").css('margin-top', 0);
+    }
+  });
+})(jQuery);
